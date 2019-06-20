b4a92d9cb678ffccff5ec2277a673316e95bb5e0
Moskize91 <i@taozeyu.com>
2019-06-20 15:37:00 +0800

    add RemoteAPI

--- app/src/main/java/com/webview/taozeyu/netlesswhiteboard/MainActivity.java
+++ app/src/main/java/com/webview/taozeyu/netlesswhiteboard/MainActivity.java
@@ -1,5 +1,7 @@
 package com.webview.taozeyu.netlesswhiteboard;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,13 +9,16 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnCreateBoard;
+    private Button btnJoinBoard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnCreateBoard = findViewById(R.id.btnCreateBoard);
-        Button btnJoinBoard = findViewById(R.id.btnJoinBoard);
+        this.btnCreateBoard = findViewById(R.id.btnCreateBoard);
+        this.btnJoinBoard = findViewById(R.id.btnJoinBoard);
 
         btnCreateBoard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,8 +35,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onClickCreateBoard() {
+        this.setEnableButtons(false);
+        RemoteAPI.instance.createRoom("test room", new RemoteAPI.Callback() {
+
+            @Override
+            public void success(final String uuid, final String roomToken) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        MainActivity.this.alert("创建房间成功", uuid + " " + roomToken);
+                    }
+                });
+            }
+
+            @Override
+            public void fail(final String errorMessage) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        MainActivity.this.setEnableButtons(true);
+                        MainActivity.this.alert("创建房间失败", errorMessage);
+                    }
+                });
+            }
+        });
     }
 
     private void onClickJoinBoard() {
+    }
+
+    private void alert(String title, String message) {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    private void setEnableButtons(boolean enable) {
+        this.btnCreateBoard.setEnabled(enable);
+        this.btnJoinBoard.setEnabled(enable);
     }
 }
