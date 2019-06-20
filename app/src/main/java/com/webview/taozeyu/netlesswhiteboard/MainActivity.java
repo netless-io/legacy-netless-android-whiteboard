761154b5ff4a6f43c47f9d1bef1d40cbc13ea382
Moskize91 <i@taozeyu.com>
2019-06-20 14:16:27 +0800

    add MainActivity

--- app/src/main/java/com/webview/taozeyu/netlesswhiteboard/MainActivity.java
+++ app/src/main/java/com/webview/taozeyu/netlesswhiteboard/MainActivity.java
@@ -2,6 +2,8 @@ package com.webview.taozeyu.netlesswhiteboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCreateBoard = findViewById(R.id.btnCreateBoard);
+        Button btnJoinBoard = findViewById(R.id.btnJoinBoard);
+
+        btnCreateBoard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.onClickCreateBoard();
+            }
+        });
+        btnJoinBoard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.onClickJoinBoard();
+            }
+        });
+    }
+
+    private void onClickCreateBoard() {
+    }
+
+    private void onClickJoinBoard() {
     }
 }
