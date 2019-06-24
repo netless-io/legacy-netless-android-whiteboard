c847c545193482df022d641f03c198c545b05f63
Moskize91 <i@taozeyu.com>
2019-06-24 17:16:11 +0800

    upgrade sdk to 2.0.3

--- app/src/main/java/com/netless/whiteboard/activity/MainActivity.java
+++ app/src/main/java/com/netless/whiteboard/activity/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
