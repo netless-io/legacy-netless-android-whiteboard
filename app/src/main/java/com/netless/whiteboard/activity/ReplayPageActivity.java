17cfdbb8da70f8fed0daaa1e9c907d92d7051a96
Moskize91 <i@taozeyu.com>
2019-06-25 13:07:35 +0800

    add seek bar

--- app/src/main/java/com/netless/whiteboard/activity/ReplayPageActivity.java
+++ app/src/main/java/com/netless/whiteboard/activity/ReplayPageActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import com.herewhite.sdk.AbstractPlayerEventListener;
@@ -14,17 +15,29 @@ import com.herewhite.sdk.WhiteSdk;
 import com.herewhite.sdk.WhiteSdkConfiguration;
 import com.herewhite.sdk.domain.DeviceType;
 import com.herewhite.sdk.domain.PlayerConfiguration;
+import com.herewhite.sdk.domain.PlayerPhase;
+import com.herewhite.sdk.domain.PlayerTimeInfo;
 import com.herewhite.sdk.domain.Promise;
 import com.herewhite.sdk.domain.SDKError;
 import com.netless.whiteboard.R;
 
 public class ReplayPageActivity extends AppCompatActivity {
 
+    private static final int SEEK_BAR_SCALE_COUNT = 1000;
+
     private Player player;
+    private long beginTimestamp;
+    private long timeDuration;
+
     private boolean didLeave = false;
+    private boolean isPlaying = false;
+    private boolean isTracking = false;
+    private long trackingTime = 0;
 
     private ProgressBar icoLoading;
+    private SeekBar seekBar;
     private Button btnGoBack;
+    private Button btnOperation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +48,15 @@ public class ReplayPageActivity extends AppCompatActivity {
 
         this.icoLoading = findViewById(R.id.icoLoading);
         this.btnGoBack = findViewById(R.id.btnGoBack);
+        this.btnOperation = findViewById(R.id.btnOperation);
+        this.seekBar = findViewById(R.id.seekBar);
 
         this.icoLoading.getIndeterminateDrawable().setColorFilter(
                 getResources().getColor(R.color.colorGrayBorder),
                 android.graphics.PorterDuff.Mode.SRC_IN
         );
+        this.setEnable(false);
+
         WhiteBroadView whiteBroadView = findViewById(R.id.whiteboard);
         WhiteSdkConfiguration configuration = new WhiteSdkConfiguration(DeviceType.touch, 10, 0.1);
         WhiteSdk whiteSdk = new WhiteSdk(whiteBroadView, this, configuration);
@@ -52,11 +69,21 @@ public class ReplayPageActivity extends AppCompatActivity {
         whiteSdk.createPlayer(playerConfiguration, new AbstractPlayerEventListener() {
 
             @Override
-            public void onLoadFirstFrame() {
+            public void onPhaseChanged(final PlayerPhase phase) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        ReplayPageActivity.this.onLoadFirstFrame();
+                        onPlayerPhaseChanged(phase);
+                    }
+                });
+            }
+
+            @Override
+            public void onScheduleTimeChanged(final long time) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        onPlayerScheduleTimeChanged(time);
                     }
                 });
             }
@@ -65,11 +92,27 @@ public class ReplayPageActivity extends AppCompatActivity {
 
             @Override
             public void then(final Player player) {
-                runOnUiThread(new Runnable() {
+                player.getPlayerTimeInfo(new Promise<PlayerTimeInfo>() {
                     @Override
-                    public void run() {
-                        ReplayPageActivity.this.player = player;
-                        setupPlayer(player);
+                    public void then(final PlayerTimeInfo playerTimeInfo) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                beginTimestamp = playerTimeInfo.getBeginTimestamp();
+                                timeDuration = playerTimeInfo.getTimeDuration();
+                                setupPlayer(player);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void catchEx(final SDKError sdkError) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                showToast(sdkError.getMessage());
+                            }
+                        });
                     }
                 });
             }
@@ -84,6 +127,26 @@ public class ReplayPageActivity extends AppCompatActivity {
                 });
             }
         });
+        this.seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (isTracking) {
+                    trackingTime = i;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                isTracking = true;
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                isTracking = false;
+                onSeekTo(trackingTime);
+            }
+        });
         this.btnGoBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -95,18 +158,54 @@ public class ReplayPageActivity extends AppCompatActivity {
                 });
             }
         });
+        this.btnOperation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        onClickOperation();
+                    }
+                });
+            }
+        });
     }
 
-    private void onLoadFirstFrame() {
-        this.icoLoading.setVisibility(View.INVISIBLE);
+    private void setEnable(boolean enable) {
+        this.btnOperation.setEnabled(enable);
     }
 
     private void setupPlayer(Player player) {
+        this.player = player;
+
         if (this.didLeave) {
             player.stop();
         } else {
-            player.play();
+            this.icoLoading.setVisibility(View.INVISIBLE);
+            this.setEnable(true);
         }
+    }
+
+    private void onPlayerPhaseChanged(PlayerPhase phase) {
+        if (phase == PlayerPhase.buffering || phase == PlayerPhase.waitingFirstFrame) {
+            this.btnOperation.setEnabled(false);
+        } else {
+            this.btnOperation.setEnabled(true);
+        }
+    }
+
+    private void onPlayerScheduleTimeChanged(long time) {
+        if (!this.isTracking) {
+            double progressRate = (double) time / (double) this.timeDuration;
+            int progress = (int) (progressRate * SEEK_BAR_SCALE_COUNT);
+            this.seekBar.setProgress(progress);
+        }
+    }
+
+    private void onSeekTo(long time) {
+        double progressRate = (double) time / (double) SEEK_BAR_SCALE_COUNT;
+        long progressTime = (int) (progressRate * this.timeDuration);
+        this.player.seekToScheduleTime(progressTime);
     }
 
     private void onClickGoBack() {
@@ -117,6 +216,15 @@ public class ReplayPageActivity extends AppCompatActivity {
         this.finish();
     }
 
+    private void onClickOperation() {
+        this.isPlaying = !this.isPlaying;
+
+        if (this.isPlaying) {
+            this.player.play();
+        } else {
+            this.player.pause();
+        }
+    }
 
     private void showToast(Object o) {
         Toast.makeText(this, o.toString(), Toast.LENGTH_SHORT).show();
